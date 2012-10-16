b62abfc2028b754df6d098453c4377fbbae55635
Naseer Ahmed <naseer@codeaurora.org>
2012-10-15 21:37:04 -0400

    msm: display: display idle checking update
    msm_fb_blank_sub might be called for UNBLANK from
    display update work queue, which is at commit state.
    Idle detection is invalid in this situation
    Add detection for external snd earlyuspend/resume interface.
    
    Change-Id: Ia12facaeb6f193c44b16926f8b1e60576db20b84
    Signed-off-by: Ken Zhang <kenz@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/msm_fb.c

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -863,7 +863,7 @@ static int msm_fb_ext_resume(struct device *dev)
 
 	if ((!mfd) || (mfd->key != MFD_KEY))
 		return 0;
-
+	msm_fb_pan_idle(mfd);
 	pdata = (struct msm_fb_panel_data *)mfd->pdev->dev.platform_data;
 	if (mfd->panel_info.type == HDMI_PANEL ||
 		mfd->panel_info.type == DTV_PANEL) {
@@ -937,6 +937,9 @@ static void msmfb_early_suspend(struct early_suspend *h)
 		wakeupflag = FALSE;
 	}
 #else
+
+        msm_fb_pan_idle(mfd);
+
 	msm_fb_suspend_sub(mfd);
 #endif
 
@@ -1054,7 +1057,8 @@ void msm_fb_set_backlight(struct msm_fb_data_type *mfd, __u32 bkl_lvl)
 	}
 #endif
 
-//	msm_fb_scale_bl(&temp);
+	msm_fb_scale_bl(&temp);
+	msm_fb_pan_idle(mfd);
 	pdata = (struct msm_fb_panel_data *)mfd->pdev->dev.platform_data;
 
 	if ((pdata) && (pdata->set_backlight)) {
@@ -1087,8 +1091,6 @@ static int msm_fb_blank_sub(int blank_mode, struct fb_info *info,
 
 	if (!op_enable)
 		return -EPERM;
-
-	msm_fb_pan_idle(mfd);
 
 	pdata = (struct msm_fb_panel_data *)mfd->pdev->dev.platform_data;
 	if ((!pdata) || (!pdata->on) || (!pdata->off)) {
